8bf8760455b7594f75202ed8ba68919d9f461ab9
diskit <diskit.1980@gmail.com>
2021-10-10 14:49:16 +0900

    add global.css

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import App from './App.svelte'
 import './container';
+// https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/faq.md#where-should-i-put-my-global-styles
+import './global.scss';
 
 const app = new App({
   target: document.getElementById('app')
